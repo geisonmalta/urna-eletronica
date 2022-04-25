33e7b96a48f486ac6197af42869cc630a65b0491
Geison <geisonmalta@hotmail.com>
2022-04-25 00:05:55 -0300

    primeiro commit

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+   {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+         {
+            numeros: '38111',
+            name: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos: [
+               {url: 'vereador01.jpg', leganda: 'Vereador', small: false}
+            ]
+         },
+         {
+            numero: '77222',
+            name: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos: [
+               {url: 'vereador02.jpg', legenda: 'Vereador'}
+            ]
+         },
+      ]
+   },
+   {
+      titulo: 'PREFEITO',  
+      numeros: 2,
+      candidatos: [
+         {
+            numero: '99',
+            name: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos: [
+               {url:'candidato01.jpg', legenda: 'Prefeito'},
+               {url:'candidatavice01.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+         },
+         {
+            numero: '84',
+            name: 'Zulano',
+            partido: 'QWERTY',
+            vice: 'Zul',
+            fotos: [
+               {url:'candidata02.jpg', legenda: 'Prefeito'},
+               {url:'candidatovice02.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+         },
+      ]
+   }
+];
